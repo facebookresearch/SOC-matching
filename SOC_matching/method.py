lambda scaling corrected in several losses, self.T corrected in models.py

SOC_matching/method.py
@@ -160,6 +160,7 @@ class NeuralSDE(torch.nn.Module):
                     gamma=self.gamma,
                     gamma2=self.gamma2,
                     scaling_factor=self.scaling_factor_M,
+                    T = self.T,
                     output_matrix=self.output_matrix,
                 ).to(self.device)
             elif algorithm in ['SOCM_diag_2B','UW_SOCM_diag_2B','SOCM_cost_diag_2B','SOCM_work_diag_2B']:
@@ -172,6 +173,7 @@ class NeuralSDE(torch.nn.Module):
                     gamma=self.gamma,
                     gamma2=self.gamma2,
                     scaling_factor=self.scaling_factor_M,
+                    T = self.T,
                     output_matrix=self.output_matrix,
                 ).to(self.device)
             elif algorithm in ['SOCM_cost_identity','SOCM_work_identity','SOCM_cost_identity_2B','SOCM_work_identity_2B','UW_SOCM_identity']:
@@ -893,6 +895,7 @@ class SOC_Solver(nn.Module):
             control_learned = -torch.einsum(
                     "ij,...j->...i", torch.transpose(self.sigma, 0, 1), nabla_V
                 )
+            # print(f'self.sigma.dtype: {self.sigma.dtype}, nabla_V.dtype: {nabla_V.dtype}, control_learned.dtype: {control_learned.dtype}, a_vectors.dtype: {a_vectors.dtype}')
             control_target = -torch.einsum(
                 "ij,...j->...i",
                 torch.transpose(self.sigma, 0, 1),
@@ -958,7 +961,11 @@ class SOC_Solver(nn.Module):
             weight = weight.detach()
 
         elif algorithm == "reinf":
-            reward = -self.lmbd * (
+            # reward = -self.lmbd * (
+            #     log_path_weight_deterministic + log_terminal_weight
+            # ).detach()
+            #### TO DEBUG lmbda ####
+            reward = -np.sqrt(self.lmbd) * (
                 log_path_weight_deterministic + log_terminal_weight
             ).detach()
             control_learned = -torch.einsum(
@@ -985,7 +992,12 @@ class SOC_Solver(nn.Module):
         #     weight = weight.detach()
 
         elif algorithm == "reinf_fr":
-            reward = -self.lmbd * (
+            # reward = -self.lmbd * (
+            #     log_path_weight_deterministic_tensor[-1,:].unsqueeze(0) - log_path_weight_deterministic_tensor[:-1,:]
+            #     + log_terminal_weight.unsqueeze(0)
+            # ).detach()
+            #### TO DEBUG lmbda ####
+            reward = -np.sqrt(self.lmbd) * (
                 log_path_weight_deterministic_tensor[-1,:].unsqueeze(0) - log_path_weight_deterministic_tensor[:-1,:]
                 + log_terminal_weight.unsqueeze(0)
             ).detach()
@@ -1107,17 +1119,29 @@ class SOC_Solver(nn.Module):
 
             nabla_control_noise = torch.autograd.grad(control_autograd_arg(self.ts, states).sum(), states)[0]
 
-            least_squares_target_integrand_term_2 = -np.sqrt(
-                self.lmbd
-            ) * torch.einsum(
+            # least_squares_target_integrand_term_2 = -np.sqrt(
+            #     self.lmbd
+            # ) * torch.einsum(
+            #     "ijkl,jml->ijmk",
+            #     M_evals[:,:-1,:,:],
+            #     nabla_control_noise[:-1,:,:],
+            # )
+            #### TO DEBUG lmbda ####
+            least_squares_target_integrand_term_2 = -torch.einsum(
                 "ijkl,jml->ijmk",
                 M_evals[:,:-1,:,:],
                 nabla_control_noise[:-1,:,:],
             )
 
-            least_squares_target_integrand_term_3 = np.sqrt(
-                self.lmbd
-            ) * torch.einsum(
+            # least_squares_target_integrand_term_3 = np.sqrt(
+            #     self.lmbd
+            # ) * torch.einsum(
+            #     "ijkl,jml->ijmk",
+            #     derivative_M_evals[:,:-1,:,:],
+            #     torch.einsum("ij,abj->abi", sigma_inverse_transpose, noises)
+            # )
+            #### TO DEBUG lmbda ####
+            least_squares_target_integrand_term_3 = torch.einsum(
                 "ijkl,jml->ijmk",
                 derivative_M_evals[:,:-1,:,:],
                 torch.einsum("ij,abj->abi", sigma_inverse_transpose, noises)
@@ -1133,6 +1157,7 @@ class SOC_Solver(nn.Module):
                 least_squares_target_integrand_term_2_3_times_sqrt_dt, dim=1
             )
 
+            # print(f'log_path_weight_deterministic_tensor.dtype: {log_path_weight_deterministic_tensor.dtype}, log_terminal_weight.dtype: {log_terminal_weight.dtype}')
             reward = - np.sqrt(self.lmbd) * (
                 log_path_weight_deterministic_tensor[-1,:].unsqueeze(0) - log_path_weight_deterministic_tensor
                 + log_terminal_weight.unsqueeze(0)
@@ -1140,15 +1165,17 @@ class SOC_Solver(nn.Module):
 
             reward_times_M_term = reward.unsqueeze(2) * cumsum_least_squares_term_2_3 
 
+            # print(f'cumsum_least_squares_term_1.dtype: {cumsum_least_squares_term_1.dtype}, least_squares_target_terminal.dtype: {least_squares_target_terminal.dtype}, reward_times_M_term.dtype: {reward_times_M_term.dtype}')
             cum_terms = cumsum_least_squares_term_1 + least_squares_target_terminal - reward_times_M_term
 
             control_learned = -torch.einsum(
                 "ij,...j->...i", torch.transpose(self.sigma, 0, 1), nabla_V
             )
+            # print(f'self.sigma.dtype: {self.sigma.dtype}, cum_terms.dtype: {cum_terms.dtype}')
             control_target = -torch.einsum(
                 "ij,...j->...i",
                 torch.transpose(self.sigma, 0, 1),
-                cum_terms,
+                cum_terms.to(self.sigma.dtype),
             )
 
             # if algorithm == "SOCM_cost":
@@ -1276,17 +1303,29 @@ class SOC_Solver(nn.Module):
 
             nabla_control_noise = torch.autograd.grad(control_autograd_arg(self.ts, states).sum(), states)[0]
 
-            least_squares_target_integrand_term_2 = -np.sqrt(
-                self.lmbd
-            ) * torch.einsum(
+            # least_squares_target_integrand_term_2 = -np.sqrt(
+            #     self.lmbd
+            # ) * torch.einsum(
+            #     "ijkl,jml->ijmk",
+            #     M_evals[:,:-1,:,:],
+            #     nabla_control_noise[:-1,:,:],
+            # )
+            #### TO DEBUG lmbda ####
+            least_squares_target_integrand_term_2 = -torch.einsum(
                 "ijkl,jml->ijmk",
                 M_evals[:,:-1,:,:],
                 nabla_control_noise[:-1,:,:],
             )
 
-            least_squares_target_integrand_term_3 = np.sqrt(
-                self.lmbd
-            ) * torch.einsum(
+            # least_squares_target_integrand_term_3 = np.sqrt(
+            #     self.lmbd
+            # ) * torch.einsum(
+            #     "ijkl,jml->ijmk",
+            #     derivative_M_evals[:,:-1,:,:],
+            #     torch.einsum("ij,abj->abi", sigma_inverse_transpose, noises)
+            # )
+            #### TO DEBUG lmbda ####
+            least_squares_target_integrand_term_3 = torch.einsum(
                 "ijkl,jml->ijmk",
                 derivative_M_evals[:,:-1,:,:],
                 torch.einsum("ij,abj->abi", sigma_inverse_transpose, noises)
@@ -1317,7 +1356,7 @@ class SOC_Solver(nn.Module):
             control_target = -torch.einsum(
                 "ij,...j->...i",
                 torch.transpose(self.sigma, 0, 1),
-                cum_terms,
+                cum_terms.to(self.sigma.dtype),
             )
 
             # if algorithm == "SOCM_cost":

SOC_matching/models.py
@@ -382,13 +382,14 @@ class DiagonalSigmoidMLP(torch.nn.Module):
             return output
     
 class TwoBoundaryScalarSigmoidMLP(torch.nn.Module):
-    def __init__(self, dim=10, hdims=[128, 128], gamma=3.0, gamma2=3.0, gamma3=10.0, scaling_factor=1.0, output_matrix=False):
+    def __init__(self, dim=10, hdims=[128, 128], gamma=3.0, gamma2=3.0, gamma3=10.0, scaling_factor=1.0, T=1.0, output_matrix=False):
         super().__init__()
 
         self.dim = dim
         self.gamma = gamma
         self.gamma2 = gamma2
         self.gamma3 = gamma3
+        self.T = T
         self.output_matrix = output_matrix
         self.sigmoid_layers = nn.Sequential(
             nn.Linear(2, hdims[0]),
@@ -408,9 +409,10 @@ class TwoBoundaryScalarSigmoidMLP(torch.nn.Module):
         ts = torch.cat((t.unsqueeze(1), s.unsqueeze(1)), dim=1)
         sigmoid_layers_output = self.sigmoid_layers(ts).unsqueeze(2)
 
-        exp_denominator = 1 / (1 - ts[:, 0])
-        exp_denominator[-1] = 1.0
+        exp_denominator = 1 / (self.T - ts[:, 0])
+        # exp_denominator[-1] = 1.0
         factor1 = torch.exp(- self.gamma3 * (ts[:, 1] - ts[:, 0]) / exp_denominator)
+        factor1 = torch.nan_to_num(factor1, nan=1.0, posinf=1.0, neginf=1.0)
 
         factor2 = (1 - torch.exp(-self.gamma * (ts[:, 1] - ts[:, 0]))) * (torch.exp(-self.gamma2 * (ts[:, 1] - ts[:, 0])) - torch.exp(-self.gamma2 * (1 - ts[:, 0])))
         identity = torch.eye(self.dim).unsqueeze(0).to(ts.device)
@@ -419,13 +421,14 @@ class TwoBoundaryScalarSigmoidMLP(torch.nn.Module):
         return output
     
 class TwoBoundaryDiagonalSigmoidMLP(torch.nn.Module):
-    def __init__(self, dim=10, hdims=[128, 128], gamma=3.0, gamma2=3.0, gamma3=10.0, scaling_factor=1.0, output_matrix=False):
+    def __init__(self, dim=10, hdims=[128, 128], gamma=3.0, gamma2=3.0, gamma3=10.0, scaling_factor=1.0, T=1.0, output_matrix=False):
         super().__init__()
 
         self.dim = dim
         self.gamma = gamma
         self.gamma2 = gamma2
         self.gamma3 = gamma3
+        self.T = T
         self.output_matrix = output_matrix
         self.sigmoid_layers = nn.Sequential(
             nn.Linear(2, hdims[0]),
@@ -448,8 +451,27 @@ class TwoBoundaryDiagonalSigmoidMLP(torch.nn.Module):
         # exp_denominator = 1 / (1 - ts[:, 0])
         # exp_denominator[-1] = 1.0
         # factor1 = torch.exp(- self.gamma3 * (ts[:, 1] - ts[:, 0]) / exp_denominator)
-        factor1 = 1 - (ts[:, 1] - ts[:, 0]) / (1 - ts[:, 0])
-        factor1[-1] = 0.0
+        # print(f'self.T: {self.T}')
+        factor1 = 1 - (ts[:, 1] - ts[:, 0]) / (self.T - ts[:, 0])
+        factor1 = torch.nan_to_num(factor1, nan=1.0, posinf=1.0, neginf=1.0)
+        # print(f'torch.max(ts[:, 1]): {torch.max(ts[:, 1])}, torch.min(ts[:, 1]): {torch.min(ts[:, 1])}')
+        # print(f'torch.max(ts[:, 0]): {torch.max(ts[:, 0])}, torch.min(ts[:, 0]): {torch.min(ts[:, 0])}')
+        # print(f'torch.max(ts[:, 1] - ts[:, 0]): {torch.max(ts[:, 1] - ts[:, 0])}, torch.min(ts[:, 1] - ts[:, 0]): {torch.min(ts[:, 1] - ts[:, 0])}')
+        # print(f'torch.max((ts[:, 1] - ts[:, 0]) / (self.T + 1e-4 - ts[:, 0])): {torch.max((ts[:, 1] - ts[:, 0]) / (self.T + 1e-4 - ts[:, 0]))}, torch.min((ts[:, 1] - ts[:, 0]) / (self.T - ts[:, 0])): {torch.min((ts[:, 1] - ts[:, 0]) / (self.T - ts[:, 0]))}')
+        # print(f'torch.max(factor1): {torch.max(factor1)}, torch.min(factor1): {torch.min(factor1)}')
+        argmax_factor1 = torch.argmax(factor1)
+        argmin_factor1 = torch.argmin(factor1)
+        # print(f'torch.argmax(factor1): {torch.argmax(factor1)}, torch.argmin(factor1): {torch.argmin(factor1)}')
+        # factor1[-1] = 0.0
+        # print(f'ts[argmax_factor1, 1]: {ts[argmax_factor1, 1]}, ts[argmax_factor1, 0]: {ts[argmax_factor1, 0]}')
+        # print(f'ts[argmin_factor1, 1]: {ts[argmin_factor1, 1]}, ts[argmin_factor1, 0]: {ts[argmin_factor1, 0]}')
+        # Count the number of NaNs
+        num_nans = torch.isnan(factor1).sum().item()
+        # print(f"Number of NaNs: {num_nans}")
+        # Find positions of NaNs
+        nan_positions = torch.nonzero(torch.isnan(factor1), as_tuple=False)
+        # print(f'nan_positions: {nan_positions}, len(factor1): {len(factor1)}')
+        # print(f'factor1[1830]: {factor1[1830]}, ts[1830, 0]: {ts[1830, 0]}, ts[1830, 1]: {ts[1830, 1]}')
         # print(f'factor1[-50:]: {factor1[-50:]}')
         # print(f'ts[-50:, 0]: {ts[-50:, 0]}, ts[-50:, 1]: {ts[-50:, 1]}')
 
@@ -457,6 +479,7 @@ class TwoBoundaryDiagonalSigmoidMLP(torch.nn.Module):
         identity = torch.eye(self.dim).unsqueeze(0).to(ts.device)
 
         output = factor1[:,None,None] * identity.repeat(ts.shape[0], 1, 1) + sigmoid_layers_output * factor2[:,None,None]
+        # print(f'TwoBoundaryDiagonalSigmoidMLP torch.mean(output): {torch.mean(output)}, torch.mean(factor1): {torch.mean(factor1)}, torch.mean(sigmoid_layers_output): {torch.mean(sigmoid_layers_output)}, torch.mean(factor2): {torch.mean(factor2)}')
         return output
 
 

main.py
@@ -520,7 +520,10 @@ def main(cfg: DictConfig):
                                 f"soc_solver.neural_sde.M.gamma: {soc_solver.neural_sde.M.gamma.item()}"
                             )
                         elif algorithm == "reinf_unadj" or algorithm == "reinf":
-                            print(f'normalization_const: {normalization_const}, norms_sqd_diff: {norm_sqd_diff.item()}, weight_mean: {weight_mean}')
+                            if compute_L2_error:
+                                print(f'normalization_const: {normalization_const}, norms_sqd_diff: {norm_sqd_diff.item()}, weight_mean: {weight_mean}')
+                            else:
+                                print(f'normalization_const: {normalization_const}, weight_mean: {weight_mean}')
                         if cfg.method.use_stopping_time:
                             print(
                                 f"torch.mean(stop_indicators): {torch.mean(stop_indicators)}"